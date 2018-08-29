6fb6f984fac32f562db81567777b8c924971997e
huyvin <hc.vincent@gmail.com>
2018-08-29 13:42:05 +0200

    database done + mvc done

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,10 +1,13 @@
 <?php
   class Pages extends Controller {
     public function __construct() {
-      $this->postModel = $this->model('Post');
+      
     }
  
     public function index() {
+
+      
+
       $this->view('pages/index', ['title' => 'welcome']);
     }
 
