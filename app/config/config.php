7e993f845c74e3c8e1a22904e6720f2ff3d6ea26
huyvin <hc.vincent@gmail.com>
2018-08-29 13:07:31 +0200

    database connection + test

--- app/config/config.php
+++ app/config/config.php
@@ -1,4 +1,10 @@
 <?php
+  //db params
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'admin');
+  define('DB_PASS', 'root');
+  define('DB_NAME', 'test');
+
   //app root
   define('APPROOT', dirname(dirname(__FILE__)));
 
